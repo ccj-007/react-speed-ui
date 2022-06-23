f7d6a5812a1922130c90e16f5d41e740ad5186c5
chen <596487930@qq.com>
2022-06-24 00:06:22 +0800

    button组件完成

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from "./components/Button/button";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button size='lg' btnType='link' href='http://www.baidu.com'>打下</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,61 @@
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
+import classNames from 'classnames'
+
+//先明确需求，定义类型
+export type ButtonSize = 'lg' | 'sm'
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+
+interface BaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+
+export const Button: FC<ButtonProps> = (props) => {
+  const {
+    className,
+    disabled,
+    size,
+    btnType,
+    children,
+    href,
+    ...restProps
+  } = props
+
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === 'link') && disabled
+  })
+
+  if (btnType === 'link' && href) {
+    return (
+      <a
+        className={classes}
+        href={href}
+        {...restProps}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        disabled={disabled}
+        {...restProps}
+      >
+        {children}
+      </button>
+    )
+  }
+
+}
