59ca46715fe26ba5f9bf205e32bd88a017d178ea
陈蔡俊 <chencaijun@mx2.zzss.com>
2022-08-13 18:39:18 +0800

    新增评分组件

--- src/components/Rate/Rate.tsx
+++ src/components/Rate/Rate.tsx
@@ -1,12 +1,48 @@
-import React, { FC } from "react";
-export interface RateProps { }
+import React, { FC, useContext, useState, ReactNode } from "react";
+import { ConfigContext } from "../Config-Provider/ConfigProvider";
+import classNames from "classnames";
 
+export interface RateProps {
+	/** 样式命名隔离 */
+	prefixCls?: string;
+	/** 组件子节点 */
+	children?: ReactNode;
+	/** 组件类名 */
+	style?: React.CSSProperties;
+	/** 组件类名 */
+	className?: string;
+	/** star总数 */
+	count?: number;
+}
+
+/**
+ * Rate 评分组件
+ */
 const Rate: FC<RateProps> = (props) => {
-  return (
-    <>
-      <div>Rate</div>
-    </>
-  );
+	const {
+		children,
+		className,
+		prefixCls: customizePrefixCls,
+		style,
+		count,
+	} = props;
+	const [state, setState] = useState(null);
+
+	const { getPrefixCls } = useContext(ConfigContext);
+	let prefixCls = getPrefixCls("rate", customizePrefixCls);
+
+	const cls = classNames(prefixCls, className, {});
+	return (
+		<div className={cls} style={style}>
+			<div className="rate-content">
+				<input type="radio" name="rate" className="speed-rate" />
+				<input type="radio" name="rate" className="speed-rate" />
+				<input type="radio" name="rate" className="speed-rate" />
+				<input type="radio" name="rate" className="speed-rate" />
+				<input type="radio" name="rate" className="speed-rate" />
+			</div>
+		</div>
+	);
 };
 
 Rate.defaultProps = {};
--- src/components/Rate/rate.stories.tsx
+++ src/components/Rate/rate.stories.tsx
@@ -1,27 +1,29 @@
 import React from "react";
 import Rate from "./Rate";
-import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import PageHeader from "../PageHeader";
 
 export default {
-  title: "数据录入/Rate",
-  component: Rate,
-  parameters: {
-    docs: {
-      description: {
-        component: "### 评分",
-      },
-    },
-  },
-} as ComponentMeta<typeof Rate>
+	title: "模板/Rate",
+	component: Rate,
+	parameters: {
+		docs: {
+			description: {
+				component: "### 组件模板",
+			},
+		},
+	},
+} as ComponentMeta<typeof Rate>;
 
-const defaultRateTpl: ComponentStory<typeof Rate> = (args) => {
-  return (
-    <>
-      <Rate></Rate>
-    </>
-  );
+const defaultRateTpl: ComponentStory<typeof Rate> = () => {
+	return (
+		<>
+			<PageHeader title="基本使用" />
+			Rate storybook
+			<Rate count={5}></Rate>
+		</>
+	);
 };
+
 export const defaultRate = defaultRateTpl.bind({});
-defaultRate.args = {
-  percent: 59,
-};
+defaultRate.args = {};
