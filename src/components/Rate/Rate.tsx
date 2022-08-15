0648a4faac04e1403108ecc02f6a84441aed02e0
陈蔡俊 <chencaijun@mx2.zzss.com>
2022-08-15 17:53:59 +0800

    完成rate

--- src/components/Rate/Rate.tsx
+++ src/components/Rate/Rate.tsx
@@ -1,19 +1,43 @@
 import React, { FC, useContext, useState, ReactNode } from "react";
 import { ConfigContext } from "../Config-Provider/ConfigProvider";
 import classNames from "classnames";
+import { css, jsx } from "@emotion/react";
 
 export interface RateProps {
 	/** 样式命名隔离 */
 	prefixCls?: string;
 	/** 组件子节点 */
 	children?: ReactNode;
-	/** 组件类名 */
+	/** 容器内联样式 */
 	style?: React.CSSProperties;
+	/** 单项内联样式 */
+	itemStyle?: React.CSSProperties;
 	/** 组件类名 */
 	className?: string;
 	/** star总数 */
 	count?: number;
+	/** 默认开始的star位置 */
+	startCount?: number;
+	/** icon的class类名 */
+	iconfontClassName?: string;
+	/** icon的class激活类名 */
+	iconfontClassNameActive?: string;
+	/** 选择后的回调 */
+	onChange?: (curIndex: number) => void;
+	/** 是否允许再次点击时清除 */
+	allowClear?: boolean;
+	/** 是否禁止 */
+	disabled?: boolean;
+	/** 自定义激活颜色 */
+	color?: string;
+	/** 自定义尺寸 */
+	size?: string;
 }
+
+interface StarType {
+	isStar: boolean;
+}
+type StartListType = StarType[];
 
 /**
  * Rate 评分组件
@@ -24,27 +48,101 @@ const Rate: FC<RateProps> = (props) => {
 		className,
 		prefixCls: customizePrefixCls,
 		style,
+		itemStyle,
 		count,
+		startCount,
+		iconfontClassName,
+		iconfontClassNameActive,
+		onChange,
+		allowClear,
+		disabled,
+		color,
+		size,
 	} = props;
-	const [state, setState] = useState(null);
+
+	//获取默认
+	const defaultStartList: StartListType = [];
+	const getDefaultList = () => {
+		if (!count || !startCount) return;
+		for (let i = 0; i < count; i++) {
+			let starTpl = {
+				isStar: startCount >= i,
+			};
+			defaultStartList.push(starTpl);
+		}
+	};
+	getDefaultList();
+
+	const [startList, setStarList] = useState(defaultStartList);
 
 	const { getPrefixCls } = useContext(ConfigContext);
 	let prefixCls = getPrefixCls("rate", customizePrefixCls);
 
 	const cls = classNames(prefixCls, className, {});
+
+	const handleStarClick = (index: number) => {
+		if (disabled) return;
+
+		let newStarList: StartListType = [];
+		const lastStarIndex = startList.filter((item) => item.isStar);
+		const lastLen = lastStarIndex.length - 1;
+		if (allowClear && lastLen === index) {
+			if (startList[index].isStar) {
+				newStarList = startList.map((item) => {
+					return { isStar: false };
+				});
+				onChange && onChange(0);
+			}
+		} else {
+			newStarList = startList.map((item, i) => {
+				let curItem = { isStar: false };
+				if (i <= index) {
+					curItem.isStar = true;
+				}
+				return curItem;
+			});
+			onChange && onChange(index);
+		}
+
+		setStarList(newStarList);
+	};
+	const handleStarCls = (item: any) => {
+		return classNames("iconfont", `${prefixCls}-item`, {
+			[`${iconfontClassNameActive}`]: item.isStar,
+			[`${iconfontClassName}`]: !item.isStar,
+			[`${prefixCls}-animation`]: item.isStar,
+		});
+	};
+
 	return (
 		<div className={cls} style={style}>
-			<div className="rate-content">
-				<input type="radio" name="rate" className="speed-rate" />
-				<input type="radio" name="rate" className="speed-rate" />
-				<input type="radio" name="rate" className="speed-rate" />
-				<input type="radio" name="rate" className="speed-rate" />
-				<input type="radio" name="rate" className="speed-rate" />
-			</div>
+			{startList.map((item, index) => {
+				return (
+					<div
+						className={handleStarCls(item)}
+						key={index}
+						style={{
+							color: startList[index].isStar ? color : "",
+							fontSize: size,
+						}}
+						onClick={() => handleStarClick(index)}
+					></div>
+				);
+			})}
+			<div className={`${prefixCls}-text`}>{children}</div>
 		</div>
 	);
 };
 
-Rate.defaultProps = {};
+Rate.defaultProps = {
+	count: 5,
+	startCount: -1,
+	iconfontClassName: "icon-star",
+	iconfontClassNameActive: "icon-star-active",
+	allowClear: false,
+	disabled: false,
+	color: "yellow",
+	size: "32px",
+};
 
 export default Rate;
--- src/components/Rate/rate.stories.tsx
+++ src/components/Rate/rate.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Rate from "./Rate";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import PageHeader from "../PageHeader";
@@ -7,23 +7,137 @@ export default {
 	title: "模板/Rate",
 	component: Rate,
 	parameters: {
-		docs: {
+		docs: {	
 			description: {
-				component: "### 组件模板",
+				component: "### 评分组件",
 			},
 		},
 	},
 } as ComponentMeta<typeof Rate>;
 
-const defaultRateTpl: ComponentStory<typeof Rate> = () => {
+/**
+ * 展示面板
+ */
+const defaultRateTpl: ComponentStory<typeof Rate> = (args) => {
+	let { count, startCount, onChange } = args;
 	return (
 		<>
 			<PageHeader title="基本使用" />
-			Rate storybook
-			<Rate count={5}></Rate>
+			<Rate
+				allowClear
+				count={count}
+				startCount={startCount}
+				onChange={onChange}
+			></Rate>
 		</>
 	);
 };
 
+const disabledRateTpl: ComponentStory<typeof Rate> = (args) => {
+	let { startCount } = args;
+	return (
+		<>
+			<PageHeader title="禁止选择" />
+			<Rate count={7} startCount={startCount} disabled></Rate>
+		</>
+	);
+};
+
+const textRateTpl: ComponentStory<typeof Rate> = (args) => {
+	let { count } = args;
+
+	const desc = ["terrible", "bad", "normal", "good", "wonderful"];
+	const [startCount, setStartCount] = useState(3);
+	const handleVal = (index) => {
+		setStartCount(index);
+	};
+	return (
+		<>
+			<PageHeader title="显示文本" />
+			<Rate count={count} startCount={startCount} onChange={handleVal}>
+				{desc[startCount]}
+			</Rate>
+
+			<PageHeader title="是否可清除" />
+			<Rate allowClear count={count} startCount={startCount}>
+				allowClear: true
+			</Rate>
+
+			<Rate count={count} startCount={startCount}>
+				allowClear: false
+			</Rate>
+		</>
+	);
+};
+
+const patternRateTpl: ComponentStory<typeof Rate> = (args) => {
+	let { startCount } = args;
+	return (
+		<>
+			<PageHeader title="其他图案" />
+			<Rate count={7} startCount={startCount}></Rate>
+			<Rate
+				count={7}
+				startCount={startCount}
+				iconfontClassName={"icon-Star"}
+				iconfontClassNameActive={"icon-Star-active"}
+			></Rate>
+			<Rate
+				count={7}
+				startCount={startCount}
+				iconfontClassName={"icon-money"}
+				iconfontClassNameActive={"icon-money-active"}
+			></Rate>
+			<Rate
+				count={7}
+				startCount={startCount}
+				iconfontClassName={"icon-ok-circle"}
+				iconfontClassNameActive={"icon-ok-circle-active"}
+			></Rate>
+
+			<PageHeader title="颜色与大小" />
+			<Rate
+				count={7}
+				startCount={startCount}
+				iconfontClassName={"icon-ok-circle"}
+				iconfontClassNameActive={"icon-ok-circle-active"}
+				color={"red"}
+			></Rate>
+			<Rate
+				count={7}
+				startCount={startCount}
+				iconfontClassName={"icon-money"}
+				iconfontClassNameActive={"icon-money-active"}
+				size={"42px"}
+			></Rate>
+		</>
+	);
+};
+
+/**
+ * 参数面板
+ */
 export const defaultRate = defaultRateTpl.bind({});
-defaultRate.args = {};
+defaultRate.args = {
+	count: 5,
+	startCount: 2,
+	onChange: (sel: number) => {
+		console.log(sel);
+	},
+};
+
+export const disabledRate = disabledRateTpl.bind({});
+disabledRate.args = {
+	startCount: 4,
+	disabled: true,
+};
+
+export const textRate = textRateTpl.bind({});
+textRate.args = {
+	count: 5,
+};
+
+export const patternRate = patternRateTpl.bind({});
+patternRate.args = {
+	count: 5,
+};
