a7daff588dfb1a4a67b426bea608ec09d5c67f1b
chen <596487930@qq.com>
2022-09-29 23:23:16 +0800

    update

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,20 +1,20 @@
 // .storybook/preview.js
 
-import "../src/styles/index.scss";
+import '../src/styles/index.scss';
 
 export const parameters = {
-	actions: { argTypesRegex: "^on[A-Z].*" },
-	controls: {
-		matchers: {
-			color: /(background|color)$/i,
-			date: /Date$/,
-		},
-	},
-	options: {
-		storySort: {
-			method: "",
-			order: ["Introduction", "Button", "Alert"],
-			locales: "",
-		},
-	},
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  options: {
+    storySort: {
+      method: '',
+      order: ['Introduction', '全局控制', '通用', '布局', '数据录入', '数据展示'],
+      locales: '',
+    },
+  },
 };
